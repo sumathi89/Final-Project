b7a24fe031f59aa41e488c7dee5825ffc5312c04
Sumathi Selvaraj <ss3467@afsconnect1.njit.edu>
2017-05-04 12:21:35 -0400

    Third commit

--- login/sign-up.php
+++ login/sign-up.php
@@ -2,26 +2,47 @@
 include 'database.php';
 if ( isset($_POST['signup']) ) {
  // clean user inputs to prevent sql injections
- $name = trim($_POST['name']);
- $name = strip_tags($name);
- $name = htmlspecialchars($name);
+ $fname = trim($_POST['fname']);
+ $fname = strip_tags($fname);
+ $fname = htmlspecialchars($fname);
+
+ $lname = trim($_POST['lname']);
+ $lname = strip_tags($lname);
+ $lname = htmlspecialchars($lname);
+
+ $uname = trim($_POST['uname']);
+ $uname = strip_tags($uanme);
+ $uname = htmlspecialchars($uname);
+
  $email = trim($_POST['email']);
  $email = strip_tags($email);
  $email = htmlspecialchars($email);
+ 
  $pass = trim($_POST['pass']);
  $pass = strip_tags($pass);
  $pass = htmlspecialchars($pass);
   // basic name validation
-  if (empty($name)) {
+  if (empty($fname)) {
   $error = true;
-  $nameError = "Please enter your full name.";
-  } else if (strlen($name) < 3) {
+  $nameError = "Please enter your first name.";
+  } else if (strlen($fname) < 3) {
   $error = true;
   $nameError = "Name must have atleat 3 characters.";
- } else if (!preg_match("/^[a-zA-Z ]+$/",$name)) {
+ }else if (!preg_match("/^[a-zA-Z ]+$/",$fname)) {
  $error = true;
- $nameError = "Name must contain alphabets and space.";
+ $nameError = "Name must contain alphabetse.";
  }
+ if (empty($lname))
+ {
+ $error=true;
+ $lnameError="Please enter your last name.";
+ }
+ else if(!preg_match("/^[a-zA-Z ]+$/",$lname))
+ {
+ $error=true;
+ $nameError="Name must contain alphabets.";
+ }
+
 //basic email validation
  if ( !filter_var($email,FILTER_VALIDATE_EMAIL) ) {
  $error = true;
